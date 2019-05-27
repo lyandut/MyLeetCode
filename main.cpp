9591a945fa553a4faad2dba63eb1045d9c3a301f
lyandut <lyan_dut@outlook.com>
2019-05-28 00:47:31 +0800

    finish 120

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
     MyLeetCode myLeetCode;
 
 //    myLeetCode.book(1, 3);
-//    cout << get<2>(myLeetCode.startEndInterval[0]);
+//    cout << get<2>(myLeetCode.startEndInterval{0});
 
 //    vector<int> A = {2,4,7,8,9,10,14,15,18,23,32,50};
 //    cout << myLeetCode.lenLongestFibSubseq(A);
@@ -20,9 +20,11 @@ int main() {
 //    vector<int> A = {14,4,5,14,13,14,10,17,2,12,2,14,7,13,14,13,4,16,4,10};
 //    myLeetCode.minIncrementForUnique(A);
 
-    vector<vector<int>> A = {{1,1,0}, {0,0,1}, {0,0,1}};
-    cout << myLeetCode.movesToChessboard(A);
+//    vector<vector<int>> A = {{1,1,0}, {0,0,1}, {0,0,1}};
+//    cout << myLeetCode.movesToChessboard(A);
 
+    vector<vector<int>> A = {{2},{3,4},{6,5,7},{4,1,8,3}};
+    cout << myLeetCode.minimumTotal(A);
     cout << endl;
     return 0;
 }
--- triangle.cpp
+++ triangle.cpp
@@ -9,9 +9,77 @@
 
 #include "MyLeetCode.h"
 
+/*
+ * 递归
+ * 42 / 43 test cases passed.
+ */
+int recursion(int i, int j, vector<vector<int>> &triangle){
+    if(i == triangle.size()-1) { return triangle[i][j]; }
+    return triangle[i][j] + min(recursion(i + 1, j, triangle), recursion(i + 1, j + 1, triangle));
+}
+
+/*
+ * DFS（回溯）
+ * 42 / 43 test cases passed.
+ */
+void dfs(int i, int j, int &sum, int &res, vector<vector<int>> &triangle){
+    sum += triangle[i][j];
+    if(i == triangle.size() - 1) {
+        res = min(res, sum);
+        return;
+    }
+    for(int k=0; k<2; ++k){
+        dfs(i + 1, j + k, sum, res, triangle);
+        sum -= triangle[i+1][j+k];
+    }
+}
+
+/*
+ * DP1
+ * 从倒数第二层开始计算，在原数组基础上修改，空间复杂度为0
+ */
+int dp1(vector<vector<int>> &triangle){
+    for(int i=triangle.size()-2; i>=0; --i){
+        for(int j=0; j<triangle[i].size(); ++j){
+            triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1]);
+        }
+    }
+    return triangle[0][0];
+}
+/*
+ * DP2
+ * 空间复杂度O(n)（n为三角形的总行数）
+ */
+int dp2(vector<vector<int>> &triangle){
+    vector<int> dp(triangle.size()+1); // 加1不用初始化最后一行
+    for (int i = triangle.size() - 1; i >= 0; --i) {
+        for (int j = 0; j < triangle[i].size(); ++j) {
+            dp[j] = min(dp[j],dp[j+1]) + triangle[i][j];
+        }
+    }
+    return dp[0];
+}
+
+/*
+ * DP2（记忆搜索）
+ * 空间复杂度O(n^2)（n为三角形的总行数）
+ */
+int recursionWithMemory(vector<vector<int>> &triangle){
+    vector<vector<int>> dp(triangle.size(), vector<int>(triangle.size()));
+    for(int i=triangle.size()-1; i>=0; --i){
+        for(int j=0; j<triangle[i].size(); ++j){
+            if(i == triangle.size()-1){
+                dp[i][j] = triangle[i][j];
+            } else{
+                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+    }
+    return dp[0][0];
+}
+
 int MyLeetCode::minimumTotal(vector<vector<int>> &triangle) {
-
-
-
-    return 0;
+    int res = INT_MAX, sum = 0;
+    dfs(0, 0, sum, res, triangle);
+    return res;
 }
