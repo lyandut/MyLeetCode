baab840aaf82ae414281405754c687470fa6ac7d
lyandut <lyan_dut@outlook.com>
2020-04-04 00:35:24 +0800

    improve 53

--- maximumSubarray.cpp
+++ maximumSubarray.cpp
@@ -10,15 +10,24 @@
 
 #include "MyLeetCode.h"
 
+// 借助原数组就地动态规划
+//int MyLeetCode::maxSubArray(vector<int> &nums) {
+//    int res = nums[0];
+//    for (int i = 1; i < nums.size(); ++i) {
+//        nums[i] = max(nums[i], nums[i - 1] + nums[i]);
+//        res = max(res, nums[i]);
+//    }
+//    return res;
+//}
+
+// 与上面是同一个方法，思路略有差异
 int MyLeetCode::maxSubArray(vector<int> &nums) {
-    vector<int> dp(nums.size()+1, INT_MIN);
-    for(int i=0; i<nums.size(); i++){
-        if(dp[i] < 0){
-            dp[i+1] = nums[i];
+    int res = nums[0];
+    for (int i = 1; i < nums.size(); ++i) {
+        if (nums[i - 1] > 0) {
+            nums[i] = nums[i - 1] + nums[i];
         }
-        else{
-            dp[i+1] = dp[i]+nums[i];
-        }
+        res = res < nums[i] ? nums[i] : res;
     }
-    return *max_element(dp.begin(), dp.end());
+    return res;
 }
